d6ccda3be8835290eac45b179461d9944af89707
SwaggyMacro <macro@julym.com>
2022-04-30 18:06:30 +0800

    添加项目文件。

---
+++ CefSharpDemo/Form1.cs
@@ -0,0 +1,67 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CefSharpDemo
+{
+    public partial class Form1 : Form
+    {
+        public class CSharpObject
+        {
+            public string getMessage(string msg)
+            {
+                string ret = $"This message return from C#: {msg}";
+                return ret;
+            }
+        }
+
+        private ChromiumWebBrowser browser;
+
+        [Obsolete]
+        public Form1()
+        {
+            InitializeComponent();
+            String url = "http://map.baidu.com/";
+            browser = new ChromiumWebBrowser(url);
+            this.Controls.Add(browser);
+            browser.Dock = DockStyle.Fill;
+            BrowserSettings browserSettings = new BrowserSettings();
+            browser.BrowserSettings = browserSettings;
+            browser.JavascriptObjectRepository.Settings.LegacyBindingEnabled = true;
+            //browser.RegisterAsyncJsObject("CSharpObject", new CSharpObject());
+            browser.JavascriptObjectRepository.Register("CSharpObject", new CSharpObject(), isAsync: false, options: BindingOptions.DefaultBinder);
+        }
+
+        async private void button1_Click(object sender, EventArgs e)
+        {
+            var javaScript = @"(function () {
+                return document.getElementsByTagName('title')[0].innerText;
+            })();";
+            var result = await browser.GetMainFrame().EvaluateScriptAsync(javaScript)
+            .ContinueWith(t =>
+            {
+                var _result = t.Result;
+                return _result.Result;
+            });
+            MessageBox.Show(result.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            browser.ExecuteScriptAsync("alert('This message from JavaScript');");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            browser.ExecuteScriptAsync("alert(CSharpObject.getMessage('Test Call'));");
+        }
+    }
+}
---
+++ CefSharpDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CefSharpDemo
+{
+    static class Program
+    {
+        private static string lib, browser, locales, res;
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            lib = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"resources\chromium\libcef.dll");
+            browser = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"resources\chromium\CefSharp.BrowserSubprocess.exe");
+            locales = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"resources\chromium\locales\");
+            res = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"resources\chromium\");
+            Console.WriteLine(lib);
+            var libraryLoader = new CefLibraryHandle(lib);
+            var isValid = !libraryLoader.IsInvalid;
+            if (isValid)
+            {
+                InitializeCef();
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        /// <summary>
+        ///     初始化Cef配置
+        /// </summary>
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void InitializeCef()
+        {
+            var settings = new CefSettings
+            {
+                BrowserSubprocessPath = browser,
+                LocalesDirPath = locales,
+                ResourcesDirPath = res
+            };
+            Cef.Initialize(settings);
+        }
+    }
+}
